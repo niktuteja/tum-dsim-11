5c41b6ee754c2453c300797e15ec263619d30195
daniel.bader@gmx.de <daniel.bader@gmx.de@139dd990-3a47-b6a4-c6dc-895d699ca6ff>
2011-05-14 18:46:28 +0000

    Initial checkin of the project.

---
+++ DiscreteEventSimulator/src/tum/des/homework/simulator/DiscreteEventSimulator.java
@@ -0,0 +1,12 @@
+package tum.des.homework.simulator;
+
+public class DiscreteEventSimulator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Whoa, look at me go!");
+	}
+}
