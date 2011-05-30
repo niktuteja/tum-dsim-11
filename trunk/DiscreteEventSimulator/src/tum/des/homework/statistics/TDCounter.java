7c9ef2440f8ce8a1cd22a4a904d9e88c63df36b8
daniel.bader@gmx.de <daniel.bader@gmx.de@139dd990-3a47-b6a4-c6dc-895d699ca6ff>
2011-05-30 11:43:57 +0000

    Added DCounter implementation and TDCounter skeleton.

---
+++ trunk/DiscreteEventSimulator/src/tum/des/homework/statistics/TDCounter.java
@@ -0,0 +1,17 @@
+package tum.des.homework.statistics;
+
+public class TDCounter extends Counter {
+
+	@Override
+	void count(double value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	double getMean() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
