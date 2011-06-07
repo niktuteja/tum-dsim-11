a2a2345c4592a5931a5e01a2cb3d337dd99217a3
daniel.bader@gmx.de <daniel.bader@gmx.de@139dd990-3a47-b6a4-c6dc-895d699ca6ff>
2011-06-07 11:49:26 +0000

    RandVarUni now takes double values.

--- trunk/DiscreteEventSimulator/src/tum/des/homework/simulator/RandVarUni.java
+++ trunk/DiscreteEventSimulator/src/tum/des/homework/simulator/RandVarUni.java
@@ -7,27 +7,26 @@ import tum.des.homework.util.Log;
 public class RandVarUni extends RandVar {
 	long min;
 	long max;
-	
 
-	public RandVarUni(String prefix, Properties p)
-	{
+	public RandVarUni(String prefix, Properties p) {
 		super(prefix, p);
-		this.min = (long)(getScale()*Long.parseLong(p.getProperty(prefix+".min", "0")));
-		this.max = (long)(getScale()*Long.parseLong(p.getProperty(prefix+".max", "1")));
+		this.min = (long) (getScale() * Double.parseDouble(p.getProperty(prefix + ".min", "0")));
+		this.max = (long) (getScale() * Double.parseDouble(p.getProperty(prefix + ".max", "1")));
 	}
-	
+
 	public RandVarUni(long min, long max) {
 		this(min, max, null);
 	}
-	
+
 	public RandVarUni(long min, long max, Long seed) {
 		super(seed);
 		this.min = min;
 		this.max = max;
 	}
 
+	@Override
 	public long getLong() {
-		long v = min + (long)(getGenerator().nextDouble() * (1 + max - min));
+		long v = min + (long) (getGenerator().nextDouble() * (1 + max - min));
 		Log.v("RandVarUni", "%d", v);
 		return v;
 	}
