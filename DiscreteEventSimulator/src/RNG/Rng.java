79a961e64d7960d809e6e05c9f0ad6d91336fd2e
daniel.bader@gmx.de <daniel.bader@gmx.de@139dd990-3a47-b6a4-c6dc-895d699ca6ff>
2011-06-20 13:40:58 +0000

    Sheet 3 Ex 1 (a)

---
+++ DiscreteEventSimulator/src/RNG/Rng.java
@@ -0,0 +1,36 @@
+package RNG;
+
+public class Rng {
+	long a; // multiplier
+	long z_0; // seed
+	long c; // increment
+	long m; // modulus
+
+	long z_new; // z_i
+	long z_old; // z_i-1
+
+	public Rng(long a, long c, long z_0, long m) {
+		this.a = a;
+		this.c = c;
+		this.z_0 = z_0;
+		this.m = m;
+
+		this.z_old = z_0;
+	}
+
+	public Rng() {
+		this(16807, 0, 1, 2147483647); // m = 2^31 - 1
+	}
+
+	public double nextDouble() {
+		z_new = (a * z_old + c) % m;
+		z_old = z_new;
+		return (double) z_new / (double) (m - 1);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Rng<a=%d, c=%d, z_0=%d, m=%d>", a, c, z_0, m);
+	}
+
+}
