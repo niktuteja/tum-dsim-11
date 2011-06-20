499f096eba81252ab5ba9d55defa73f66c9654b0
vincenz.doelle@googlemail.com <vincenz.doelle@googlemail.com@139dd990-3a47-b6a4-c6dc-895d699ca6ff>
2011-06-20 20:45:08 +0000

    started ex 2

--- trunk/DiscreteEventSimulator/src/RandVar/Exponential.java
+++ trunk/DiscreteEventSimulator/src/RandVar/Exponential.java
@@ -1,116 +1,123 @@
 package RandVar;
 
+import RNG.Rng;
+
 /**
  * Discrete Event Simulation WS2010/2011
- *
- * Exponential class
- * Generates exponential distributed samples
- *
+ * 
+ * Exponential class Generates exponential distributed samples
+ * 
  * @author Alexander Klein
- * @version 1.0.0 
+ * @version 1.0.0
  * @since 2010-11-08
  */
 
-public class  Exponential extends RandVar
-{
+public class Exponential extends RandVar {
 	/**
 	 * Attribute: mean = 1 / lambda
 	 */
 	public double lambda;
 
 	/**
-	 * Constructor of the Exponential class
-	 * lambda is initialized with the value 1
-	 * => mean = 1
+	 * Constructor of the Exponential class lambda is initialized with the value
+	 * 1 => mean = 1
 	 */
-	public Exponential ()
-	{
+	public Exponential() {
 		lambda = 1;
 	}
 
 	/**
 	 * Constructor of the Exponential class
-	 *@param la the lambda value
+	 * 
+	 * @param la
+	 *            the lambda value
 	 */
-	public Exponential (double la)
-	{
+	public Exponential(double la) {
 		lambda = la;
+	}
+
+	/**
+	 * Constructor of the Exponential class
+	 * 
+	 * @param la
+	 *            the lambda value
+	 */
+	public Exponential(double la, Rng rng) {
+		lambda = la;
+		this.rng = rng;
 	}
 
 	/**
 	 * Returns the name of the distribution
 	 */
-	public String type()
-	{
+	public String type() {
 		return "exponential distribution";
 	}
 
 	/**
 	 * Function generates a double value according to the set attributes
-	 *@return the sample
+	 * 
+	 * @return the sample
 	 */
-	public double getRV () 
-	{
-        return (- (Math.log (rng.nextDouble()) * ( 1 / lambda) ));
-    }
+	public double getRV() {
+		double rv = (-(Math.log(rng.nextDouble()) * (1 / lambda)));
+		return rv;
+	}
 
 	/**
 	 * Method return the mean of the distribution as double value
-	 *@return the mean
+	 * 
+	 * @return the mean
 	 */
-	public double getMean() 
-	{ 
-		return (1 / lambda); 
+	public double getMean() {
+		return (1 / lambda);
 	}
 
 	/**
 	 * Method calculates the variance of the distribution as double value
-	 *@return the variance
+	 * 
+	 * @return the variance
 	 */
-	public double getVariance() 
-	{
+	public double getVariance() {
 		return (1 / (lambda * lambda));
 	}
-	
+
 	/**
 	 * Method sets the mean according to the given parameter
-	 *@param the new mean value
+	 * 
+	 * @param the
+	 *            new mean value
 	 */
-	public void setMean(double m)
-	{
-        if (m > 0)
-		{
-            lambda = (1 / m);
-        }
-		else
-		{
-            System.out.println("mean > 0 required");
+	public void setMean(double m) {
+		if (m > 0) {
+			lambda = (1 / m);
+		} else {
+			System.out.println("mean > 0 required");
 		}
-    }
+	}
 
 	/**
 	 * Interface
 	 */
-	public void setMeanAndStdDeviation(double m, double s)
-	{
-        System.out.println("not applicable");
-    }
+	public void setMeanAndStdDeviation(double m, double s) {
+		System.out.println("not applicable");
+	}
 
-    /**
+	/**
 	 * Interface
 	 */
-    public void setStdDeviation(double s){
-        System.out.println("not applicable");
-    }
-	
+	public void setStdDeviation(double s) {
+		System.out.println("not applicable");
+	}
+
 	/**
-	 * Function prints the statistical characteristics of the random variable to the console
+	 * Function prints the statistical characteristics of the random variable to
+	 * the console
 	 */
-	public void report ()
-	{
+	public void report() {
 		System.out.println(type());
 		System.out.println("parameters:");
-		System.out.println("lambda = " + lambda);		
-		super.report ();
+		System.out.println("lambda = " + lambda);
+		super.report();
 	}
 }
