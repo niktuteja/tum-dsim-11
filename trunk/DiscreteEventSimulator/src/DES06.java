9b41ed18d420c9d3893b22fb05dd67acad04efcc
daniel.bader@gmx.de <daniel.bader@gmx.de@139dd990-3a47-b6a4-c6dc-895d699ca6ff>
2011-07-11 13:42:13 +0000

    Imported official solution for Sheet 6.

--- trunk/DiscreteEventSimulator/src/DES06.java
+++ trunk/DiscreteEventSimulator/src/DES06.java
@@ -2,120 +2,190 @@ import Analysis.*;
 import Simulator.*;
 import RandVar.*;
 
-public class DES06 {
+/**
+ * Exercise 6		Discrete Event Simulation SS 2011
+ *
+ * DES06 class
+ * Main class performing the simulation.
+ *
+ * @author Alexander Klein
+ * @version 1.0.0 
+ * @since 2011-07-06
+ */
+public class  DES06
+{
 	/**
-	 * main. Application entry point for DES exercise
+	 * main. Application entry point for DES exercise 6
 	 * 
-	 * @param args
-	 *            if no arguments are given, all subsections will run.
+	 * @param args 
+	 * args[0] indicates the exercise 1 or 2
+	 * Exercise 1: args[1] = lazyThreshold
+	 * Exercise 1: args[2] = speedFactor
+	 * Exercise 2: args[1] specifies the simulated system
 	 */
-	public static void main(String[] args) {
-		exercise1a();
-	}
-
-	private static void exercise1a() {
-		// System A:-------------- M / M / 1 - Infty -----------------------
-		long real_time_to_sim_time = 100;
-		long sctNormal = 8;
-		RandVar iat = new Exponential((double) 10 / real_time_to_sim_time);
-		iat.setSeed(1);
-		RandVar sct = new Exponential((double) sctNormal
-				/ real_time_to_sim_time);
-		sct.setSeed(15);
-
-		long simulation_duration = (long) (Math.pow(10, 5) * real_time_to_sim_time);
-		long maxQueueSize = Long.MAX_VALUE;
-		long preferablePlaces = -1;
+	public static void main(String[] args) 
+	{
+		Rng a = new Rng (1, 16807, 0, (1 << 31) - 1);
+		Rng b = new Rng (1, 16807, 15, (1 << 31) - 1);
+		Rng c = new Rng (1, 16807, 100, (1 << 31) - 1);
+		RandVar iat, sct, biat = null, batchSize = null;
+				
+		// Exercise 1
+		if (args.length == 3 || args[0].equals("1")) {
+			long   lazyThreshold = new Long(args[1]).longValue();
+			double speedFactor = new Double(args[2]).doubleValue();
+			
+			//System A:-------------- M / M / 1 - Infty -----------------------
+			long real_time_to_sim_time = 100;
+			iat = new Exponential((double) 10 / real_time_to_sim_time);
+			iat.rng = a;
+			sct = new Exponential((double) 8 / real_time_to_sim_time);
+			sct.rng = b;
+			
+			long simulation_duration = (long) (Math.pow(10,5)*real_time_to_sim_time);
+			long maxQueueSize = Long.MAX_VALUE;
+			
+			SimState.s = new SimState ( iat, sct, simulation_duration, maxQueueSize, lazyThreshold, speedFactor, real_time_to_sim_time);
+			runSimulation(args[0]);
+		}
 		
-		long lazyCashierThreshold = 10;
-		double lazyCashierSpeedUp = 0.50;
-
-		RandVar sctSpeedUp = new Exponential((double) (sctNormal + sctNormal
-				* lazyCashierSpeedUp)
-				/ real_time_to_sim_time);
-		sct.setSeed(15);
-
-		SimState.s = new SimState(iat, sct, simulation_duration, maxQueueSize,
-				preferablePlaces, lazyCashierThreshold, lazyCashierSpeedUp,
-				sctSpeedUp, real_time_to_sim_time);
-		SimState.s.ec.insert(new CustomerArrival(0));
-		runSimulation();
+		
+		
+		// Exercise 2 - args[0] Exercise - args[1] - system
+		if (args.length == 2 || args[0].equals("2")) {
+			
+			long real_time_to_sim_time = 100;
+			
+			//System A
+			if (args[1].equals("a"))
+			{
+				biat = new Exponential((double) 9 / real_time_to_sim_time);
+				batchSize = new Constant ((double) 1);
+			}
+			//System B
+			if (args[1].equals("b"))
+			{
+				biat = new Exponential((double) 4.5 / real_time_to_sim_time);
+				batchSize = new Constant ((double) 2);
+			}
+			//System C
+			if (args[1].equals("c"))
+			{
+				biat = new Exponential((double) 3 / real_time_to_sim_time);
+				batchSize = new Constant ((double) 3);
+			}
+			//System D
+			if (args[1].equals("d"))
+			{
+				biat = new Exponential((double) 9 / (double) 4 / real_time_to_sim_time);
+				batchSize = new Constant ((double) 4);
+			}
+			//System E
+			if (args[1].equals("e"))
+			{
+				biat = new Exponential((double) 9 / (double) 5 / real_time_to_sim_time);
+				batchSize = new Constant ((double) 5);
+			}
+			//System F
+			if (args[1].equals("f"))
+			{
+				biat = new Exponential((double) 9 / (double) 5 / real_time_to_sim_time);
+				batchSize = new Uniform ((double) 1, (double) 10);
+			}
+			//System Test
+			if (args[1].equals("test"))
+			{
+				biat = new Exponential((double) 9 / (double) 5 / real_time_to_sim_time);
+				batchSize = new Uniform ((double) 10, (double) 20);
+			}
+			sct = new Exponential((double) 10 / real_time_to_sim_time);
+			
+			//Set Rng's
+			biat.rng = a;
+			sct.rng = b;
+			batchSize.rng = c;
+			
+			long simulation_duration = (long) (Math.pow(10,5)*real_time_to_sim_time);
+			long maxQueueSize = Long.MAX_VALUE;
+			
+			SimState.s = new SimState (null, sct, biat, batchSize, simulation_duration, maxQueueSize, -1, 0, real_time_to_sim_time);
+			
+			runSimulation(args[0]);
+		}
 	}
-	
-	private static void exercise2a() {
-		// System A:-------------- M / M / 1 - Infty -----------------------
-		long real_time_to_sim_time = 100;
-		RandVar iat = new Exponential((double) 5 / real_time_to_sim_time);
-		iat.setSeed(1);
-		RandVar sct = new Exponential((double) 10 / real_time_to_sim_time);
-		sct.setSeed(15);
-
-		long simulation_duration = (long) (Math.pow(10, 5) * real_time_to_sim_time);
-		RandVar na = new Uniform(1, 10);
-		// RandVar na = new Constant(5);
-		SimState.s = new SimState(iat, sct, na, simulation_duration,
-				real_time_to_sim_time);
-		SimState.s.ec.insert(new BatchArrival(0, Math.round(na.getRV())));
-		runSimulation();
-	}
-	
-	private static void runSimulation() {
-		// Initialize CounterCollection
+		
+	static void runSimulation(String args) 
+	{
+		//Initialize CounterCollection
 		CounterCollection.cc = new CounterCollection();
 
-		// Insert the first and the last event
-		SimState.s.ec.insert(new SimulationTermination(
-				SimState.s.simulationDuration));
-
+		//Insert the first event:
+		//CustomerArrival for exercise 1 or
+		//BatchArrival for exercise 2
+		if (args.equals("1"))
+			SimState.s.ec.insert (new CustomerArrival (0));
+		else
+			SimState.s.ec.insert (new BatchArrival (0));
+		
+		//Insert termination event
+		SimState.s.ec.insert (new SimulationTermination(SimState.s.simulationDuration));
+		
 		/**
 		 * Simulation is done here
 		 */
-
-		while (SimState.s.stop != true) {
+		
+		while (SimState.s.stop != true) {	
 			// Get the next SimEvent from the EventChain
-			SimEvent e = (SimEvent) SimState.s.ec.removeOldestEvent();
-
+			SimEvent e = (SimEvent) SimState.s.ec.removeOldestEvent ();
+			
 			if (e != null) {
-				if (SimState.s.now > e.value) {
-					System.out
-							.println("_____________________________________________________");
+				if (SimState.s.now > e.value) 
+				{
+					System.out.println("_____________________________________________________");
 					System.out.println("ERROR: Time warp detected");
 					System.out.println("Event Time" + e.value);
 					System.out.println("System Time" + SimState.s.now);
-					System.out
-							.println("_____________________________________________________");
+					System.out.println("_____________________________________________________");
 				}
-
 				SimState.s.now = e.value;
-
+				
 				e.process();
-			} else {
+			} 
+			else 
+			{
 				System.out.println("EventChain is empty! Simulation aborted!");
 				SimState.s.stop = true;
 			}
-
+			
 			// Updating the min and the max attribute of the SimState
-			SimState.s.min = (SimState.s.queue.size() < SimState.s.min ? SimState.s.queue
-					.size() : SimState.s.min);
-			SimState.s.max = (SimState.s.queue.size() > SimState.s.max ? SimState.s.queue
-					.size() : SimState.s.max);
-
+			SimState.s.min = (SimState.s.queue.size() < SimState.s.min ? SimState.s.queue.size():SimState.s.min);
+			SimState.s.max = (SimState.s.queue.size() > SimState.s.max ? SimState.s.queue.size():SimState.s.max);
+					
 		}
-
-		// report general simulation stats
-		System.out.println("E[inter-arrival time] = "
-				+ SimState.s.iat.getMean());
-		System.out.println("E[service completion time] = "
-				+ SimState.s.sct.getMean());
-		System.out.println("simulation duration = "
-				+ SimState.s.simulationDuration);
-		System.out.println("minimum queue length = " + SimState.s.min);
-		System.out.println("maximum queue length = " + SimState.s.max);
-		// Report includes histograms
+		
+		// report general simulation stats - stats are chosen depending on the exercise
+		if (args.equals("1"))
+		{
+			System.out.println("E[inter-arrival time] = " + SimState.s.iat.getMean());
+			System.out.println("E[service completion time] = " + SimState.s.sct.getMean());
+			System.out.println("simulation duration = " + SimState.s.simulationDuration);
+			System.out.println("minimum queue length = " + SimState.s.min);
+			System.out.println("maximum queue length = " + SimState.s.max);
+		}
+		else
+		{
+			System.out.println("E[batch-inter-arrival time] = " + SimState.s.biat.getMean());
+			System.out.println("E[batch-size] = " + SimState.s.batchSize.getMean());
+			System.out.println("E[inter-arrival time] = " + SimState.s.biat.getMean()/SimState.s.batchSize.getMean());
+			System.out.println("E[service completion time] = " + SimState.s.sct.getMean());
+			System.out.println("simulation duration = " + SimState.s.simulationDuration);
+			System.out.println("minimum queue length = " + SimState.s.min);
+			System.out.println("maximum queue length = " + SimState.s.max);
+		}	
+		//Report includes histograms
 		CounterCollection.cc.report();
-		CounterCollection.cc.fileReport();
-		System.out
-				.println("___________________________________________________________________");
-
+		System.out.println("___________________________________________________________________");
+			
+		
 	}
 }
--- trunk/DiscreteEventSimulator/src/Simulator/BatchArrival.java
+++ trunk/DiscreteEventSimulator/src/Simulator/BatchArrival.java
@@ -2,14 +2,23 @@ package Simulator;
 
 import Analysis.*;
 
+/**
+ * BatchArrival is a special SimEvent.
+ * At least one CustomerArrival in the EventChain is needed to 
+ * start the simulation.
+ * @author Alexander Klein
+ * @version 1.0.1 
+ * @since 2011-07-070
+ */
 public class BatchArrival extends SimEvent
 {
-	private final long numArrivals;
-	
-	public BatchArrival (long time, long numArrivals)
+	/**
+	 * Constructor that uses the given argument
+	 *@param time Representing the simulation time at which the event occurs
+	 */
+	public BatchArrival (long time)
 	{
 		value = time;
-		this.numArrivals = numArrivals;
 	}
 	/**
 	 * Function describes the system behavior depending on the serverBusy attribute 
@@ -17,14 +26,76 @@ public class BatchArrival extends SimEvent
 	 */
 	public void process ()
 	{
-		long iat = Math.round(SimState.s.iat.getRV());
-		long na = Math.round(SimState.s.na.getRV());
-		SimState.s.ec.insert (new BatchArrival(SimState.s.now + iat, na));
+		/**
+		 * If server is not busy insert the CustomerArrival in the server and set him to busy state
+		 */
+		long biat = Math.round(SimState.s.biat.getRV());
 		
-		for (int i = 0; i < numArrivals; i++)
+		//Insert the next BatchArrival in the EventChain
+		SimState.s.ec.insert ((SimEvent) new BatchArrival (SimState.s.now + biat));
+		
+		//Remove expired customers first! Otherwise expired customers can block new ones
+		if (SimState.s.cet != null)
+		{	
+			SimState.s.removeExpiredCustomers ();
+		}		
+		
+		double batchSize;
+		if (SimState.s.biat != null) 
+			batchSize = SimState.s.batchSize.getRV();
+		else
+			batchSize = 0;
+		
+		//Note: The iat of the last customer is given by the biat
+		for (int i = 0; i < batchSize;i++)
 		{
-			SimState.s.ec.insert ((SimEvent) new CustomerArrival (SimState.s.now));
-			CounterCollection.cc.dc_ciat.count((double) SimState.s.now / SimState.s.real_time_to_sim_time);
+			if ( i < (batchSize-1) )
+				insertCustomer (0);
+			else
+				insertCustomer (biat);
 		}
+	}
+	public void insertCustomer (double iat)
+	{
+		Customer c = new Customer ();
+		c.arrivalTime = SimState.s.now;
+		if (SimState.s.cet != null) 
+		{
+			c.deadline = SimState.s.now+Math.round(SimState.s.cet.getRV());
+		}
+	
+	
+		if (SimState.s.serverBusy == false) 
+		{
+			//Insert the ServiceCompletion event in the EventChain
+			SimState.s.ec.insert ((SimEvent) new ServiceCompletion (SimState.s.now + Math.round(SimState.s.sct.getRV())));
+
+			SimState.s.serverBusy = true;
+			c.serviceInitTime = SimState.s.now;
+		
+			SimState.s.customerInServer = c;
+			CounterCollection.cc.dc_bp.count(0);
+		}
+		/**
+		 * If server is busy try to insert the CustomerArrival in the queue
+		 */
+		else
+		{
+			if (SimState.s.queue.size() < SimState.s.maxQueueSize)
+			{
+				SimState.s.queue.add(c);
+				CounterCollection.cc.dc_bp.count(0);
+			}
+			else
+			{
+				CounterCollection.cc.dc_bp.count(1);
+			}
+		}
+		CounterCollection.cc.dc_ciat.count((double) iat / SimState.s.real_time_to_sim_time);
+				
+		// measurement	
+		CounterCollection.cc.cc_su.count(1);
+		CounterCollection.cc.cc_qo.count(SimState.s.queue.size());	
+		CounterCollection.cc.ch_qo.count(SimState.s.queue.size());	
 	}
 }
